104f64b32e3420970a5f15e10fb0367bdcc3f367
Bob den Otter <bobdenotter@gmail.com>
2023-02-21 17:43:09 +0100

    working

---
+++ src/Controller/QuestController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestController extends AbstractController
+{
+    #[Route('/quest', name: 'app_quest')]
+    public function index(): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED');
+        
+        return $this->render('quest/index.html.twig', [
+            'controller_name' => 'QuestController',
+        ]);
+    }
+}
