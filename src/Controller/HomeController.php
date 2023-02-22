7d1f32481b13a883db4fba5a008cd792c2e43962
Bob den Otter <bobdenotter@gmail.com>
2023-02-22 15:39:33 +0100

    First quests

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use DiceBag\Dice\Dice;
+use DiceBag\DiceBag;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +13,11 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        $roll = DiceBag::factory('4d6dl1 + 3d8+4');
+        dump($roll->getTotal());
+        dump($roll->getDicePools());
+
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
